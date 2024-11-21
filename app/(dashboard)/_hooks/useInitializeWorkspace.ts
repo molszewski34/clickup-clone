4fe17254575dbca1e0fddb6cc4b8c697d5e8bf86
molszewski34 <m.olszewski341@gmail.com>
2024-11-21 12:49:22 +0100

    feat: added hook to initialize createDefaultWorkspace when page is loaded

---
+++ app/(dashboard)/_hooks/useInitializeWorkspace.ts
@@ -0,0 +1,12 @@
+import { useEffect } from 'react';
+import { auth } from '@/db/firebase/lib/firebase';
+import { createDefaultWorkspace } from '@/app/server-actions/createDefaultWorkspace';
+
+export const useInitializeWorkspace = () => {
+  useEffect(() => {
+    const user = auth.currentUser;
+    if (user) {
+      createDefaultWorkspace(user.uid);
+    }
+  }, []);
+};
