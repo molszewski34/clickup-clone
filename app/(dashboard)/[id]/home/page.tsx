c60d6fd21c6473c86d47af2149bc3e83bf59f898
molszewski34 <m.olszewski341@gmail.com>
2024-11-25 17:33:36 +0100

    feat: implemented TopBar and AddWorkspace components

--- app/(dashboard)/[id]/home/page.tsx
+++ app/(dashboard)/[id]/home/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 
-import { use, useEffect } from 'react';
+import { use, useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '@/db/firebase/lib/firebase';
 import { logoutUser } from '../../utils/logoutUser';
 import { useInitializeWorkspace } from '../../_hooks/useInitializeWorkspace';
 
+import TopbarNav from '@/app/topBar-Nav/components/TopbarNav';
+import AddWorkspace from '../../_components/AddWorkspace';
 
 interface UserHomeProps {
   params: Promise<{ id: string }>;
@@ -30,8 +32,10 @@ const UserHomePage: React.FC<UserHomeProps> = ({ params }) => {
 
   return (
     <div>
+      <TopbarNav />
       <h1>Użytkownik ma id: {id}!</h1>
       <p>Witaj w swoim dashboard</p>
+      <AddWorkspace />
       <button onClick={logoutUser}>Wyloguj</button>
     </div>
   );
