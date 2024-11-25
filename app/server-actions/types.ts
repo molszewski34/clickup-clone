cb7209663156a17ac19d54313c427ed2614b4e09
molszewski34 <m.olszewski341@gmail.com>
2024-11-25 17:42:01 +0100

    feat: created types
    Some types are not used yet and were created as reminder

---
+++ app/server-actions/types.ts
@@ -0,0 +1,38 @@
+export interface Task {
+  name: string;
+  projectId: string;
+  taskId: string;
+  createdAt: Date;
+  status: string;
+  dueDate?: Date | null;
+  assignees?: Assignee[];
+  timeEstimate?: string;
+  priority?: string;
+  details?: string;
+}
+
+export interface Assignee {
+  uid: string;
+  name?: string;
+  email?: string;
+}
+
+export interface Project {
+  id?: string;
+  name?: string;
+  createdAt?: string;
+  desc?: string;
+  userId?: string;
+  workspaceId?: string;
+  isPrivate?: boolean;
+}
+
+export interface Workspace {
+  id: string;
+  name: string;
+  createdAt: string;
+  userId: string;
+  desc: string;
+  icon: string;
+  isPrivate: boolean;
+}
