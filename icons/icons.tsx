2416bfc64d4b53f375c04dcc24f97925ab375d6b
molszewski34 <m.olszewski341@gmail.com>
2024-11-26 11:50:05 +0100

    feat: updated file with new icons BsLayoutTextSidebar, FiHome

--- icons/icons.tsx
+++ icons/icons.tsx
@@ -1,24 +1,24 @@
-import { SvgIcons } from "./svgIcons";
-import { FaListUl,FaPlus } from "react-icons/fa6";
-import { RiDashboardHorizontalLine } from "react-icons/ri";
-import { HiOutlineCalendar } from "react-icons/hi2";
-import { IoIosArrowDown } from "react-icons/io";
-import { FiSearch } from "react-icons/fi";
-import { LuSettings } from "react-icons/lu";
-
-
-
+import { SvgIcons } from './svgIcons';
+import { FaListUl, FaPlus } from 'react-icons/fa6';
+import { RiDashboardHorizontalLine } from 'react-icons/ri';
+import { HiOutlineCalendar } from 'react-icons/hi2';
+import { IoIosArrowDown } from 'react-icons/io';
+import { FiSearch } from 'react-icons/fi';
+import { LuSettings } from 'react-icons/lu';
+import { BsLayoutTextSidebar } from 'react-icons/bs';
+import { FiHome } from 'react-icons/fi';
 export const Icons = {
-    ListUlicon:FaListUl,
-    CalendarIcon:HiOutlineCalendar,
-    DashboardIcon:RiDashboardHorizontalLine,
-    BoardIcon:SvgIcons.Board,
-    TeableIcon:SvgIcons.Table,
-    GantIcon:SvgIcons.HorizontalChart,
-    PlusIco:FaPlus,
-    SliderHorizontal:SvgIcons.SlidersHorizontal,
-    ArrowDownIcon:IoIosArrowDown,
-    SearchIcon:FiSearch,
-    SettingsIcon:LuSettings,
-
+  ListUlicon: FaListUl,
+  CalendarIcon: HiOutlineCalendar,
+  DashboardIcon: RiDashboardHorizontalLine,
+  BoardIcon: SvgIcons.Board,
+  TeableIcon: SvgIcons.Table,
+  GantIcon: SvgIcons.HorizontalChart,
+  PlusIco: FaPlus,
+  SliderHorizontal: SvgIcons.SlidersHorizontal,
+  ArrowDownIcon: IoIosArrowDown,
+  SearchIcon: FiSearch,
+  SettingsIcon: LuSettings,
+  SidebarExpandIcon: BsLayoutTextSidebar,
+  HomePageIndicatorIcon: FiHome,
 };
