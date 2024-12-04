70eba1853f7db64b83b8f21184ed72b69893b9fe
molszewski34 <m.olszewski341@gmail.com>
2024-12-04 08:43:26 +0100

    #8696yb9gy feat: created tasks query

---
+++ hooks/useTaskQuery.ts
@@ -0,0 +1,19 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
+import { getProjects } from '@/app/server-actions/project/getProjects';
+import { useData } from '@/context/DataProvider/DataProvider';
+import { getTasks } from '@/app/server-actions/task/getTasks';
+
+export const useTaskQuery = () => {
+  const { userId, workspaceId, projectId } = useData();
+
+  if (!userId) {
+    return { data: [], error: 'User ID is not available' };
+  }
+
+  return useQuery({
+    queryKey: ['projects', userId, workspaceId, projectId],
+    queryFn: () => getTasks(userId, workspaceId, projectId),
+  });
+};
