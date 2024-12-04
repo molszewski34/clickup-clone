52d42d2f607600bd4f93e34879cb5cc66e84eca3
molszewski34 <m.olszewski341@gmail.com>
2024-12-04 11:19:22 +0100

    feat: added new icons

--- icons/icons.tsx
+++ icons/icons.tsx
@@ -1,12 +1,21 @@
 import { SvgIcons } from './svgIcons';
-import { FaListUl, FaPlus } from 'react-icons/fa6';
+import { FaListUl, FaPlus, FaRegCircleDot } from 'react-icons/fa6';
 import { RiDashboardHorizontalLine } from 'react-icons/ri';
 import { HiOutlineCalendar } from 'react-icons/hi2';
-import { IoIosArrowDown } from 'react-icons/io';
+import { IoIosArrowDown, IoMdClose } from 'react-icons/io';
+import { IoIosArrowUp } from 'react-icons/io';
 import { FiSearch } from 'react-icons/fi';
 import { LuSettings } from 'react-icons/lu';
 import { BsLayoutTextSidebar } from 'react-icons/bs';
 import { FiHome } from 'react-icons/fi';
+import { AiOutlineTeam } from 'react-icons/ai';
+import { HiOutlineLogout } from 'react-icons/hi';
+import { IoIosMore } from 'react-icons/io';
+import { VscLayoutCentered } from 'react-icons/vsc';
+import { GoCodeSquare } from 'react-icons/go';
+import { PiArrowLineDownRight, PiStarBold } from 'react-icons/pi';
+import { CiImageOn } from 'react-icons/ci';
+import { CiShare2 } from 'react-icons/ci';
 export const Icons = {
   ListUlicon: FaListUl,
   CalendarIcon: HiOutlineCalendar,
@@ -17,8 +26,20 @@ export const Icons = {
   PlusIco: FaPlus,
   SliderHorizontal: SvgIcons.SlidersHorizontal,
   ArrowDownIcon: IoIosArrowDown,
+  ArrowUpIcon: IoIosArrowUp,
   SearchIcon: FiSearch,
   SettingsIcon: LuSettings,
   SidebarExpandIcon: BsLayoutTextSidebar,
   HomePageIndicatorIcon: FiHome,
+  TeamIcon: AiOutlineTeam,
+  Logout: HiOutlineLogout,
+  IoIosMore: IoIosMore,
+  Star: PiStarBold,
+  LayoutCentered: VscLayoutCentered,
+  Close: IoMdClose,
+  CodeSquare: GoCodeSquare,
+  PiArrowLineDownRight: PiArrowLineDownRight,
+  FaRegCircleDot: FaRegCircleDot,
+  CiImageOn: CiImageOn,
+  CiShare2: CiShare2,
 };
