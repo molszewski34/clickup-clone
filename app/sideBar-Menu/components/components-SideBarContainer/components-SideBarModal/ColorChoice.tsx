ccbe5e5350f798d1554997fbf5bc7140cbdad74f
molszewski34 <m.olszewski341@gmail.com>
2024-12-07 10:22:17 +0100

    #8696yqwah added setFormData from provider and func to pass activeColor: color to icon obj

--- app/sideBar-Menu/components/components-SideBarContainer/components-SideBarModal/ColorChoice.tsx
+++ app/sideBar-Menu/components/components-SideBarContainer/components-SideBarModal/ColorChoice.tsx
@@ -1,31 +1,40 @@
-"use client"; // Informuje o tym, że komponent działa po stronie klienta w Next.js
+'use client'; // Informuje o tym, że komponent działa po stronie klienta w Next.js
 
-import React, { useState } from "react"; // Importujemy React i useState
-import { ColorChoiceProps } from "./types"; // Importujemy typy z pliku types
+import React, { useState } from 'react'; // Importujemy React i useState
+import { ColorChoiceProps } from './types'; // Importujemy typy z pliku types
+import { useWorkspaceFormContext } from '@/context/DataProvider/FormProviders/WorkspaceFormProvider';
+import { Workspace } from '@/app/server-actions/types';
 
 // Komponent ColorChoice przyjmuje onColorChange jako prop
 const ColorChoice: React.FC<ColorChoiceProps> = ({ onColorChange }) => {
-  const [activeColor, setActiveColor] = useState<string>("indigo-500"); // Stan do przechowywania aktywnego koloru
+  const [activeColor, setActiveColor] = useState<string>('indigo-500'); // Stan do przechowywania aktywnego koloru
 
   // Lista dostępnych kolorów do wyboru
   const colors: string[] = [
-    "indigo-500",
-    "blue-500",
-    "sky-500",
-    "teal-500",
-    "emerald-600",
-    "amber-500",
-    "orange-500",
-    "red-500",
-    "pink-500",
-    "purple-500",
-    "stone-500",
-    "black",
+    'indigo-500',
+    'blue-500',
+    'sky-500',
+    'teal-500',
+    'emerald-600',
+    'amber-500',
+    'orange-500',
+    'red-500',
+    'pink-500',
+    'purple-500',
+    'stone-500',
+    'black',
   ];
+
+  const { setFormData } = useWorkspaceFormContext();
 
   // Funkcja obsługująca zmianę koloru
   const handleColorChange = (color: string) => {
     setActiveColor(color); // Ustawiamy wybrany kolor jako aktywny
+
+    setFormData((prevState: Workspace) => ({
+      ...prevState,
+      icon: [...prevState.icon, { activeColor: color }],
+    }));
     if (onColorChange) onColorChange(color); // Jeśli istnieje funkcja onColorChange, wywołujemy ją z nowym kolorem
   };
 
@@ -42,7 +51,7 @@ const ColorChoice: React.FC<ColorChoiceProps> = ({ onColorChange }) => {
               className={`flex float-left items-center justify-center rounded-full w-[28px] h-[28px] ${
                 activeColor === color
                   ? `border-2 border-${color} cursor-default` // Aktywny kolor, dodajemy obramowanie
-                  : "cursor-pointer hover:border-2 hover:border-gray-200" // Inaczej, zmiana kursora i obramowanie przy hover
+                  : 'cursor-pointer hover:border-2 hover:border-gray-200' // Inaczej, zmiana kursora i obramowanie przy hover
               } bg-white`}
             >
               {/* Wyświetlamy kolor w przycisku */}
