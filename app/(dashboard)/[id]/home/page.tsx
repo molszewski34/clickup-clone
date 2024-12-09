f71171ed1c0d6c7f2e554fc98d2af9ef126c408f
BroksDMG <kubaskrzeczowski00@gmail.com>
2024-12-09 20:30:16 +0100

    widgetHeader component done

--- app/(dashboard)/[id]/home/page.tsx
+++ app/(dashboard)/[id]/home/page.tsx
@@ -10,6 +10,8 @@ import PageIndicator from "../../ui/PageIndicator";
 import { Icons } from "@/icons/icons";
 import WidgetHeader from "../../_components/WidgetHeader";
 import ViewsBarContainer from "../../_components/ViewsBarContainer";
+import ButtonVariant3 from "@/components/ButtonVariant3";
+import ButtonVariant2 from "@/components/ButtonVariant2";
 
 const UserHomePage = ({ params }: { params: Promise<{ id: string }> }) => {
   const router = useRouter();
@@ -38,11 +40,22 @@ const UserHomePage = ({ params }: { params: Promise<{ id: string }> }) => {
 
   return (
     <div className="w-full">
-      <WidgetHeader>
-        <Icons.HomeIcon />
-        Home
+      <WidgetHeader className="justify-between">
+        <div className="flex px-2 items-center">
+          <Icons.HomeIcon className="mr-1" />
+          Home
+        </div>
+        <div className="flex items-center px-2 gap-1">
+          <ButtonVariant3 className={` !text-sm h-8 font-semibold px-[11px]`}>
+            Menage cards
+          </ButtonVariant3>
+          <div className="w-[1px] h-4 mx-2  bg-darkGray_400"></div>
+          <ButtonVariant2 className={`items-center h-8`}>
+            <Icons.SettingsIcon className="text-base" />
+          </ButtonVariant2>
+        </div>
       </WidgetHeader>
-      <ViewsBarContainer />
+      {/* <ViewsBarContainer /> */}
       <PageNavbar>
         <PageIndicator icon={<Icons.HomePageIndicatorIcon />} name="Home" />
       </PageNavbar>
--- app/(dashboard)/_components/WidgetHeader.tsx
+++ app/(dashboard)/_components/WidgetHeader.tsx
@@ -5,10 +5,10 @@ type Props = {
   className?: string;
 };
 
-const WidgetHeader = ({ children, className = "p-2" }: Props) => {
+const WidgetHeader = ({ children, className = "" }: Props) => {
   return (
     <div
-      className={`h-[49px] flex items-center  transition-all duration-500 ease-in-out
+      className={`h-[49px] p-2 flex items-center  transition-all duration-500 ease-in-out
          text-white_100 bg-nav border-gray_100 border-b dark:bg-darkGray_600 font-sans text-sm 
          ${className}`}
     >
