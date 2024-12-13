115bbcb193f1b402d6ca5ec641ac64bf34c0a03c
molszewski34 <m.olszewski341@gmail.com>
2024-12-13 12:50:08 +0100

    #869720y0z created testing logs and changed queries

--- app/(dashboard)/[id]/home/components/TaskTable/Table.tsx
+++ app/(dashboard)/[id]/home/components/TaskTable/Table.tsx
@@ -1,21 +1,32 @@
-"use client";
-import React, { ReactNode, useMemo, useState } from "react";
-import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
-import { TaskTableRow } from "./TaskTableRow";
-import { Icons } from "@/icons/icons";
-import { FlatTaskElement, TaskElement, TaskStatus } from "../../types";
-import { flattenTableData } from "../../_helpers/flattenTableData";
-import AddTaskToTableDownside from "./AddTaskToTableDownside/AddTaskToTableDownside";
+'use client';
+import React, { ReactNode, useMemo, useState } from 'react';
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+import { TaskTableRow } from './TaskTableRow';
+import { Icons } from '@/icons/icons';
+import { FlatTaskElement, TaskElement, TaskStatus } from '../../types';
+import { flattenTableData } from '../../_helpers/flattenTableData';
+import AddTaskToTableDownside from './AddTaskToTableDownside/AddTaskToTableDownside';
 
-export const Table = ({ tasks, status }: { tasks: TaskElement[]; status: TaskStatus }) => {
+export const Table = ({
+  tasks,
+  status,
+}: {
+  tasks: TaskElement[];
+  status: TaskStatus;
+}) => {
   const [expandedTasks, setExpandedTasks] = useState<string[]>([]);
   const columns: ColumnDef<FlatTaskElement, ReactNode>[] = [
     {
-      accessorKey: "title",
+      accessorKey: 'title',
       header: () => <div className="text-left">Name</div>,
     },
     {
-      accessorKey: "assignees",
+      accessorKey: 'assignees',
       header: () => <div className="text-left w-[76px]">Assignee</div>,
       cell: (props) => (
         <div className="flex text-left items-center max-w-24 h-full hover:outline hover:outline-1 hover:outline-gray-400 hover:rounded hover:outline-offset-1 hover:cursor-pointer">
@@ -24,7 +35,7 @@ export const Table = ({ tasks, status }: { tasks: TaskElement[]; status: TaskSta
       ),
     },
     {
-      accessorKey: "dueDate",
+      accessorKey: 'dueDate',
       header: () => <div className="text-left w-[90px]">Due date</div>,
       cell: () => (
         <div className="flex text-left items-center max-w-24 h-full hover:outline hover:outline-1 hover:outline-gray-400 hover:rounded hover:outline-offset-1 hover:cursor-pointer">
@@ -33,7 +44,7 @@ export const Table = ({ tasks, status }: { tasks: TaskElement[]; status: TaskSta
       ),
     },
     {
-      accessorKey: "priority",
+      accessorKey: 'priority',
       header: () => <div className="text-left w-[76px]">Priority</div>,
       cell: (props) => (
         <div className="flex text-left text-sm items-center max-w-24 h-full hover:outline hover:outline-1 hover:outline-gray-400 hover:rounded hover:outline-offset-1 hover:cursor-pointer">
@@ -43,7 +54,7 @@ export const Table = ({ tasks, status }: { tasks: TaskElement[]; status: TaskSta
       ),
     },
     {
-      accessorKey: "status",
+      accessorKey: 'status',
       header: () => <div className="text-left w-[100px]">Status</div>,
       cell: (props) => (
         <div className="flex text-left text-sm items-center max-w-24 h-full hover:outline hover:outline-1 hover:outline-gray-400 hover:rounded hover:outline-offset-1 hover:cursor-pointer">
@@ -52,7 +63,7 @@ export const Table = ({ tasks, status }: { tasks: TaskElement[]; status: TaskSta
       ),
     },
     {
-      accessorKey: "comments",
+      accessorKey: 'comments',
       header: () => <div className="text-left w-[76px]">Comments</div>,
       cell: () => (
         <div className="flex items-center max-w-24 h-full hover:outline hover:outline-1 hover:outline-gray-400 hover:rounded hover:outline-offset-1 hover:cursor-pointer">
@@ -61,7 +72,7 @@ export const Table = ({ tasks, status }: { tasks: TaskElement[]; status: TaskSta
       ),
     },
     {
-      accessorKey: "addField",
+      accessorKey: 'addField',
       header: () => <div className="text-left w-[76px]">Add field</div>,
       cell: () => (
         <div className="flex items-center max-w-24 h-full hover:outline hover:outline-1 hover:outline-gray-400 hover:rounded hover:outline-offset-1 hover:cursor-pointer">
@@ -71,10 +82,10 @@ export const Table = ({ tasks, status }: { tasks: TaskElement[]; status: TaskSta
     },
   ];
 
-  const flatTasks = useMemo(() => flattenTableData(tasks, null, 0), [tasks]);
+  // const flatTasks = useMemo(() => flattenTableData(tasks, null, 0), [tasks]);
 
   const table = useReactTable({
-    data: flatTasks,
+    data: tasks,
     columns,
     getCoreRowModel: getCoreRowModel(),
   });
@@ -90,8 +101,12 @@ export const Table = ({ tasks, status }: { tasks: TaskElement[]; status: TaskSta
               <th
                 key={header.id}
                 colSpan={header.colSpan}
-                className="font-normal text-gray-500 hover:bg-gray-200 hover:cursor-pointer border-b p-1">
-                {flexRender(header.column.columnDef.header, header.getContext())}
+                className="font-normal text-gray-500 hover:bg-gray-200 hover:cursor-pointer border-b p-1"
+              >
+                {flexRender(
+                  header.column.columnDef.header,
+                  header.getContext()
+                )}
               </th>
             ))}
           </tr>
@@ -100,7 +115,7 @@ export const Table = ({ tasks, status }: { tasks: TaskElement[]; status: TaskSta
       <tbody className="">
         {table.getRowModel().rows.map((singleRow) => {
           if (singleRow.original.level !== 0) {
-            if (expandedTasks.includes(singleRow.original.parentId || "")) {
+            if (expandedTasks.includes(singleRow.original.parentId || '')) {
               return (
                 <TaskTableRow
                   key={singleRow.id}
--- app/(dashboard)/[id]/l/[projectId]/page.tsx
+++ app/(dashboard)/[id]/l/[projectId]/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Table } from '../../home/components/TaskTable/Table';
 import { MOCK_TASKS } from '../../home/data';
 import { TaskStatus } from '../../home/types';
+import TaskList from './components/TasksList';
 
 export default async function Page({
   params,
@@ -10,22 +11,9 @@ export default async function Page({
 }) {
   await params;
 
-  const tasksInProgress = MOCK_TASKS.filter(
-    (task) => task.status === TaskStatus.inProgress
-  );
-  const tasksTodo = MOCK_TASKS.filter(
-    (task) => task.status === TaskStatus.todo
-  );
-  const tasksCompleted = MOCK_TASKS.filter(
-    (task) => task.status === TaskStatus.completed
-  );
-
   return (
     <div className="flex flex-col gap-4">
-      {/* Example for using the Table with filtered tasks */}
-      <Table tasks={tasksCompleted} status={TaskStatus.completed} />
-      <Table tasks={tasksInProgress} status={TaskStatus.inProgress} />
-      <Table tasks={tasksTodo} status={TaskStatus.todo} />
+      <TaskList />
     </div>
   );
 }
