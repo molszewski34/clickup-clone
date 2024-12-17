dc97e2e181d1bc69ece653fe1b19f9c1c74c8f04
molszewski34 <m.olszewski341@gmail.com>
2024-12-17 11:47:02 +0100

    #8697420d8 fix added error states for validation reset

--- app/sideBar-Menu/components/components-SideBarContainer/components-SideBarModal/SpaceModal.tsx
+++ app/sideBar-Menu/components/components-SideBarContainer/components-SideBarModal/SpaceModal.tsx
@@ -1,32 +1,35 @@
-"use client";
+'use client';
 
-import React, { useState, useRef, useEffect } from "react";
-import { ModalProps } from "@/app/topBar-Nav/components/type";
-import SpaceModalHeader from "./components-SpaceModal/SpaceModalHeader";
-import SpaceModalBody from "./components-SpaceModal/SpaceModalBody";
-import SpaceModalFooter from "./components-SpaceModal/SpaceModalFooter";
+import React, { useState, useRef, useEffect } from 'react';
+import { ModalProps } from '@/app/topBar-Nav/components/type';
+import SpaceModalHeader from './components-SpaceModal/SpaceModalHeader';
+import SpaceModalBody from './components-SpaceModal/SpaceModalBody';
+import SpaceModalFooter from './components-SpaceModal/SpaceModalFooter';
+import { useWorkspaceFormContext } from '@/context/FormProviders/WorkspaceFormProvider';
 
 export default function SpaceModal({ onClose }: ModalProps) {
   const [isModalVisible, setModalVisible] = useState(false);
-  const [selectedColor, setSelectedColor] = useState("indigo-500");
-  const [selectedIcon, setSelectedIcon] = useState<string>("");
+  const [selectedColor, setSelectedColor] = useState('indigo-500');
+  const [selectedIcon, setSelectedIcon] = useState<string>('');
 
   const modalRef = useRef<HTMLDivElement | null>(null);
+  const { setError } = useWorkspaceFormContext();
 
   const handleOutsideClick = (event: MouseEvent) => {
     if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
       setModalVisible(false);
+      setError(false);
     }
   };
 
   useEffect(() => {
     if (isModalVisible) {
-      document.addEventListener("mousedown", handleOutsideClick);
+      document.addEventListener('mousedown', handleOutsideClick);
     } else {
-      document.removeEventListener("mousedown", handleOutsideClick);
+      document.removeEventListener('mousedown', handleOutsideClick);
     }
     return () => {
-      document.removeEventListener("mousedown", handleOutsideClick);
+      document.removeEventListener('mousedown', handleOutsideClick);
     };
   }, [isModalVisible]);
 
