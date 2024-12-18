a92b17ea4db65a078f238f3f9d57f836446ef036
molszewski34 <m.olszewski341@gmail.com>
2024-12-18 10:25:35 +0100

    fix: return to older version by ovewrating new

--- app/(dashboard)/[id]/docs/page.tsx
+++ app/(dashboard)/[id]/docs/page.tsx
@@ -1,15 +1,10 @@
-"use client";
+'use client';
 
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "@/db/firebase/lib/firebase";
-import { useInitializeWorkspace } from "../../_hooks/useInitializeWorkspace";
-import { Icons } from "@/icons/icons";
-import WidgetHeader from "../../_components/WidgetHeader";
-import ButtonVariant3 from "@/components/ButtonVariant3";
-import ButtonVariant2 from "@/components/ButtonVariant2";
-import HomeContent from "@/app/homeSpaceContent/HomeContent";
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '@/db/firebase/lib/firebase';
+import { useInitializeWorkspace } from '../../_hooks/useInitializeWorkspace';
 
 const UserDocPage = ({ params }: { params: Promise<{ id: string }> }) => {
   const router = useRouter();
@@ -24,7 +19,7 @@ const UserDocPage = ({ params }: { params: Promise<{ id: string }> }) => {
 
       const unsubscribe = onAuthStateChanged(auth, (user) => {
         if (!user || user.uid !== userId) {
-          router.push("/login");
+          router.push('/login');
         }
       });
 
@@ -37,28 +32,11 @@ const UserDocPage = ({ params }: { params: Promise<{ id: string }> }) => {
   }, [params, router]);
 
   return (
-    <div className="w-full">
-      <WidgetHeader className="justify-between">
-        <div className="flex px-2 items-center">
-          <Icons.HomeIcon className="mr-1" />
-          Home
-        </div>
-        <div className="flex items-center px-2 gap-1">
-          <ButtonVariant3 className={` !text-sm h-8 font-semibold px-[11px]`}>
-            Menage cards
-          </ButtonVariant3>
-          <div className="w-[1px] h-4 mx-2  bg-gray_50"></div>
-          <ButtonVariant2 className={`items-center h-8`}>
-            <Icons.SettingsIcon className="text-base" />
-          </ButtonVariant2>
-        </div>
-      </WidgetHeader>
-      <div
-        className="w-full custom-scrollbar overflow-y-auto overflow-x-hidden"
-        style={{ height: "calc(100vh - 90px)" }}
-      >
-        <HomeContent />
-      </div>
+    <div
+      className="flex justify-center items-center w-full font-sans text-4xl"
+      style={{ height: 'calc(100vh - 40px)' }}
+    >
+      Page Docs in progress
     </div>
   );
 };
--- hooks/useCreateWorkspace.tsx
+++ hooks/useCreateWorkspace.tsx
@@ -1,8 +1,10 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { createNewWorkspace } from '@/app/server-actions/workspace/createNewWorkspace';
 import { Workspace } from '@/app/server-actions/types';
+import { useWorkspaceFormContext } from '@/context/FormProviders/WorkspaceFormProvider';
 
 export const useCreateWorkspace = () => {
+  const { setFormData } = useWorkspaceFormContext();
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: async ({
@@ -15,6 +17,10 @@ export const useCreateWorkspace = () => {
       await createNewWorkspace(formData, userId);
     },
     onSuccess: () => {
+      setFormData((prevState: Workspace) => ({
+        ...prevState,
+        id: crypto.randomUUID(),
+      }));
       queryClient.invalidateQueries({ queryKey: ['workspaces'] });
       console.log('Workspace created successfully!');
     },
