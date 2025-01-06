0052f7526a1c7bb9eb8864f0817becdedb696169
molszewski34 <m.olszewski341@gmail.com>
2025-01-06 21:24:59 +0100

    CU-8697b72zn switched location from where User type is imported

--- app/(dashboard)/[id]/home/components/TaskTable/UserInitials.tsx
+++ app/(dashboard)/[id]/home/components/TaskTable/UserInitials.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { getInitials } from "../../../l/[projectId]/utils/getInitials";
-import { User } from "@/app/server-actions/user/getUser";
+import { User } from "@/app/server-actions/types";
 
 type UserInitialsProps = {
   user: User;
@@ -10,7 +10,8 @@ export const UserInitials = ({ user }: UserInitialsProps) => {
   return (
     <div
       className="flex items-center justify-center bg-violet-500 h-6 w-6 border rounded-full text-xs font-normal text-gray-200 -mx-0.5 cursor-pointer"
-      data-tooltip-target="tooltip-default">
+      data-tooltip-target="tooltip-default"
+    >
       {getInitials(user.signUpFullName)}
     </div>
   );
