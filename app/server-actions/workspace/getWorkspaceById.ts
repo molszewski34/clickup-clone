0ea0e575ba62a084fd57c0951e60179a6d439858
Karol Słupiński <karol.slupinski.97@gmail.com>
2025-02-13 14:33:28 +0100

    [fix] return id when reading user or workspace

--- app/server-actions/workspace/getWorkspaceById.ts
+++ app/server-actions/workspace/getWorkspaceById.ts
@@ -3,14 +3,14 @@ import { Workspace } from "../types";
 import { db } from "@/db/firebase/lib/firebase";
 
 export const getWorkspaceById = async (workspaceId: Workspace["id"]) => {
-  const workspaceRef = doc(db, `workspace/${workspaceId}`);
   try {
+    const workspaceRef = doc(db, `workspace/${workspaceId}`);
     const workspaceDoc = await getDoc(workspaceRef);
     if (!workspaceDoc.data()) {
       console.error("Workspace not found");
       return;
     }
-    return workspaceDoc.data() as Workspace;
+    return { id: workspaceDoc.id, ...workspaceDoc.data() } as Workspace;
   } catch (error) {
     console.error("Error occured when fetching space", error);
   }
