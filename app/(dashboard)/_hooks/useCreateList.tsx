766f219b5c6b64c9824ef3cd52b7e379a0a52cb4
Mariusz Olszewski <m.olszewski341>
2025-03-18 11:52:15 +0100

    CU-8698c88kh feat: new create list handlers

---
+++ app/(dashboard)/_hooks/useCreateList.tsx
@@ -0,0 +1,50 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { useData } from "@/context/DataProvider/DataProvider";
+import { useRouter } from "next/navigation";
+import { useUser } from "@/context/DataProvider/UserDataProvider";
+import { createList } from "@/app/server-actions/List/createList";
+import { useUsersGetUserById } from "@/hooks/useUsersGetUserById";
+
+interface UseCreateListProps {
+  toggleModal: (modal: "none" | "menuSpaceList" | "createList") => void;
+}
+
+export const useCreateList = ({ toggleModal }: UseCreateListProps) => {
+  const router = useRouter();
+  const { spaceId } = useData();
+  const { userId } = useUser();
+  const queryClient = useQueryClient();
+  const { setListId } = useData();
+  const { data: userData } = useUsersGetUserById(userId);
+
+  const workspaceId = userData?.activeWorkspace;
+  if (!workspaceId) {
+    throw new Error("Workspace is undefined");
+  }
+  return useMutation({
+    mutationFn: async ({
+      listName,
+      isPrivate,
+    }: {
+      listName: string;
+      isPrivate: boolean;
+    }) => {
+      return await createList(workspaceId, listName, spaceId, isPrivate);
+    },
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ["lists"] });
+
+      toggleModal("none");
+
+      router.push(`/${workspaceId}/l/${data?.id}`);
+      if (!data) {
+        throw new Error("List not found");
+      }
+      setListId(data.id);
+    },
+    onError: (error: unknown) => {
+      console.error("Error creating list:", error);
+    },
+  });
+};
