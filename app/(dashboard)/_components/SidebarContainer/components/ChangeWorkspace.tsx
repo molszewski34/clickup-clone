a21b2695d97c943bb5d16e69cfee055411645c20
Mariusz Olszewski <m.olszewski341>
2025-04-30 08:45:14 +0200

    CU-8698wxnhf added conditional rendering which checks if user is Admin

--- app/(dashboard)/_components/SidebarContainer/components/ChangeWorkspace.tsx
+++ app/(dashboard)/_components/SidebarContainer/components/ChangeWorkspace.tsx
@@ -5,13 +5,17 @@ import { useChangeUserActiveWorkspace } from "@/hooks/useChangeUserActiveWorkspa
 import { WorkspaceWithMembers } from "@/hooks/useGetWorkspacesForUser";
 import { Workspace } from "@/app/server-actions/types";
 import { useUser } from "@/context/DataProvider/UserDataProvider";
+import useUserAssociation from "@/hooks/useUserAssociation";
 
 type ChangeWorkspaceProps = {
   workspacesToSwitch: WorkspaceWithMembers[];
   currentWorkspace: Workspace;
 };
 
-export const ChangeWorkspace = ({ workspacesToSwitch, currentWorkspace }: ChangeWorkspaceProps) => {
+export const ChangeWorkspace = ({
+  workspacesToSwitch,
+  currentWorkspace,
+}: ChangeWorkspaceProps) => {
   const { userId } = useUser();
   const { mutate } = useChangeUserActiveWorkspace();
 
@@ -23,27 +27,37 @@ export const ChangeWorkspace = ({ workspacesToSwitch, currentWorkspace }: Change
     window.location.href = `/${currentWorkspace.id}/setting/profile`;
   };
 
+  const { userAssociation } = useUserAssociation();
+  const userRole = userAssociation?.role;
+
+  console.log("user role", userRole);
   return (
     <>
       <div className="flex flex-col h-fit p-2">
         {/* <WorkspaceCard workspaceName="Karol Słupiński's workspace" numberOfMembers={1} /> */}
         <button
           className="flex items-center gap-2 w-full px-2 py-1 text-gray-700 text-sm font-normal hover:bg-gray-200 rounded-md"
-          onClick={handleButtonSettingsClick}>
+          onClick={handleButtonSettingsClick}
+        >
           <Icons.SettingsIcon className="text-gray-500 text-[16px] " />
           Settings
         </button>
-        <button
-          className="flex items-center gap-2 w-full px-2 py-1 text-gray-700 text-sm font-normal hover:bg-gray-200 rounded-md"
-          onClick={handleButtonClick}>
-          <Icons.PeopleIcon className="text-gray-500 text-[16px] " />
-          Manage users
-        </button>
+        {userRole !== undefined && userRole == "Admin" && (
+          <button
+            className="flex items-center gap-2 w-full px-2 py-1 text-gray-700 text-sm font-normal hover:bg-gray-200 rounded-md"
+            onClick={handleButtonClick}
+          >
+            <Icons.PeopleIcon className="text-gray-500 text-[16px] " />
+            Manage users
+          </button>
+        )}
       </div>
       <div className="w-full h-px bg-gray-200"></div>
       <div className="Flex flex-col p-2 max-h-80 overflow-y-auto">
         <div className="flex justify-between items-center gap-2 p-2">
-          <div className="text-[12px] font-medium text-gray-500">Switch Workspace</div>
+          <div className="text-[12px] font-medium text-gray-500">
+            Switch Workspace
+          </div>
           {/* <button className="flex items-center p-1 hover:bg-gray-200 rounded-md">
             <Icons.SearchIcon className="text-gray-500 text-[14px] " />
           </button> */}
@@ -54,7 +68,10 @@ export const ChangeWorkspace = ({ workspacesToSwitch, currentWorkspace }: Change
               <button
                 className="flex items-center hover:bg-gray-200 rounded-md"
                 key={`workspace-card-${singleWorkspace.id}`}
-                onClick={() => mutate({ userId, newActiveWorkspaceId: singleWorkspace.id })}>
+                onClick={() =>
+                  mutate({ userId, newActiveWorkspaceId: singleWorkspace.id })
+                }
+              >
                 <WorkspaceCard
                   workspaceName={singleWorkspace.name}
                   workspaceNumberOfMembers={singleWorkspace.members.length}
