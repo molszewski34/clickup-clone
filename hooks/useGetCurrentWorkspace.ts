4d0d7cefda07bc2a4ae937d8bcfb4dd167f71d93
Mariusz Olszewski <m.olszewski341>
2025-05-27 10:43:55 +0200

    CU-86996b3xk następne logi do debugowania po stronie klienta

--- hooks/useGetCurrentWorkspace.ts
+++ hooks/useGetCurrentWorkspace.ts
@@ -3,10 +3,19 @@ import { useUsersGetUserById } from "./useUsersGetUserById";
 
 function useGetCurrentWorkspace() {
   const { userId } = useGetCurrentUser();
+  console.log("[useGetCurrentWorkspace] userId:", userId);
 
-  const { data: userData } = useUsersGetUserById(userId ?? "");
+  const {
+    data: userData,
+    isLoading,
+    error,
+  } = useUsersGetUserById(userId ?? "");
+  console.log("[useGetCurrentWorkspace] userData:", userData);
+  console.log("[useGetCurrentWorkspace] isLoading:", isLoading);
+  console.log("[useGetCurrentWorkspace] error:", error);
 
   const workspaceId = userData?.activeWorkspace;
+  console.log("[useGetCurrentWorkspace] workspaceId:", workspaceId);
 
   return { workspaceId };
 }
